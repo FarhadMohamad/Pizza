46ac3818aa47fa869eaa49e9221194295328dfeb
FarhadMohamad <art_ismat@hotmail.com>
2019-05-30 11:46:07 +0200

    new

---
+++ data/pizzas.js
@@ -0,0 +1,42 @@
+// this is hardcored data since It takes time to backup everytime
+exports.pizzas = [
+
+{
+
+   0: {
+        id: 0,
+        name: "Italian",
+        desctiption: "Tasty pizza with olives and mushrooms",
+        sizes: {
+        small: "1.99",
+        medium: "2.99" ,
+        large: "3.99" 
+        }
+    },
+
+   1: {
+        id: 1,    
+        name: "hawaiian",
+        desctiption: "Tasty pizza with pinaples",
+        sizes: {
+            small: "1.99",
+            medium: "2.99" ,
+            large: "3.99" 
+            }
+    },
+  2:
+    {
+        id: 2,
+        name: "pepperoni",
+        desctiption: "Tasty pizza with pepperoni",
+        sizes: {
+            small: "1.99",
+            medium: "2.99" ,
+            large: "3.99" 
+            }
+    }
+    
+}
+]
+    
+
